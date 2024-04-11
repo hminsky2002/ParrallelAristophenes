a7c3de69f285f84da094c43d213e5f43e6a4d9fd
Harry Minsky <harry.minsky@gmail.com>
2024-04-10 22:31:24 -0400

    Poorly Documented But Functional Implementation

--- primes.c
+++ primes.c
@@ -1,92 +1,118 @@
-#include <mpi.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
+#include <math.h>
+#include <stdbool.h>
+#include <mpi.h>
  
- 
+//this method creates an array of size N with all values initialized to 1,
+//to represent their potential prime-ness
+bool *createBaseArray(int N) {
+  bool *base_array = (bool *)malloc(sizeof(bool) * N);
+  for (int i = 0; i < N; i++) {
+    base_array[i] = true;
+  }
+  return base_array;
+}
+
+//this method creates a sub array of size N with all values uninitialized
+bool *createSubArray(int n) {
+  bool *subArray = (bool *)malloc(sizeof(bool) * n);
+  return subArray;
+}
+
 int main(int argc, char* argv[])
 {
-    int val = 12;
-    int my_rank, comm_sz, elements_per_process;
+    int N = atoi(argv[1])+1;
+    
+    int my_rank, comm_sz;
+
     MPI_Status status;
 
-    MPI_Init(&argc, &argv);
- 
+    double t1,t2;
+    MPI_Init(NULL, NULL);
+    
     MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
     MPI_Comm_size(MPI_COMM_WORLD, &comm_sz);
- 
-    if (my_rank == 0) {
-        int start, i;
-        elements_per_process = val/comm_sz;
- 
-        
-        if (comm_sz > 1) {
-         
-            for (i = 1; i < comm_sz - 1; i++) {
-                start = i * elements_per_process;
- 
-                MPI_Send(&elements_per_process,
-                         1, MPI_INT, i, 0,
-                         MPI_COMM_WORLD);
-                MPI_Send(&start,
-                         1,
-                         MPI_INT, i, 0,
-                         MPI_COMM_WORLD);
-            }
- 
-           
-            int p = i * elements_per_process;
-            int s = val - elements_per_process;
- 
-            MPI_Send(&s,
-                     1, MPI_INT,
-                     i, 0,
-                     MPI_COMM_WORLD);
-            MPI_Send(&p,
-                     1,
-                     MPI_INT, i, 0,
-                     MPI_COMM_WORLD);
-        }
- 
-        int sum = 0;
-        for (i = 1; i < elements_per_process; i++)
-            sum += i;
- 
-        int tmp;
-        for (i = 1; i < comm_sz; i++) {
-            MPI_Recv(&tmp, 1, MPI_INT,
-                     MPI_ANY_SOURCE, 0,
-                     MPI_COMM_WORLD,
-                     &status);
-            int sender = status.MPI_SOURCE;
- 
-            sum += tmp;
-        }
- 
-        printf("Sum of array is : %d\n", sum);
-    }
-    else {
-        int z, e;
-        MPI_Recv(&e,
-                 1, MPI_INT, 0, 0,
-                 MPI_COMM_WORLD,
-                 &status);
- 
 
-        MPI_Recv(&z, 1,
-                 MPI_INT, 0, 0,
-                 MPI_COMM_WORLD,
-                 &status);
- 
-        int partial_sum = 0;
-        for (int i = 1; i < e; i++)
-            partial_sum += (z+i);
- 
-        MPI_Send(&partial_sum, 1, MPI_INT,
-                 0, 0, MPI_COMM_WORLD);
+    bool *base_array = NULL;
+    if (my_rank == 0 || true) {
+        t1 = MPI_Wtime(); 
+        base_array = createBaseArray(N);
     }
- 
+    int *send_counts = malloc(sizeof(int)*comm_sz);
+    int *displs = malloc(sizeof(int)*comm_sz);
+    if(N < comm_sz){
+        for(int i = 0; i < comm_sz; i++){
+            if(i <= N){
+               send_counts[i] = 1;
+            }
+            else{
+               send_counts[i] = 0;
+            }
+        }
+        for(int i = 0; i < comm_sz; i++){
+            displs[i]=0;
+        }
+
+    }
+    else{
+        int subset_offset = N % comm_sz;
+        int trimmed_count = N - subset_offset;
+        int set_size = trimmed_count/comm_sz;
+
+        for(int i = 0; i < comm_sz; i++){
+            if(i == (comm_sz-1) ){
+                send_counts[i] = set_size+subset_offset;
+            }
+            else{
+                send_counts[i] = set_size;
+            }
+        }
+        displs[0] = 0;
+        for(int i = 1; i < comm_sz; i++){
+            displs[i]= displs[i-1] + send_counts[i-1];
+        }
+    }
+
+    int limit = (int)ceil(sqrt(N));
+    int start;
+    bool *sub_array = createSubArray(send_counts[my_rank]);
+
+    for(int i = 2; i < limit+1; i++){
+        start = displs[my_rank];
+
+        MPI_Scatterv(base_array, send_counts, displs, MPI_C_BOOL, sub_array,
+            send_counts[my_rank],  MPI_C_BOOL, 0, MPI_COMM_WORLD);
+
+        for(int j = 0; j < send_counts[my_rank]; j++){
+            if(start+j > 2  && (start+j) != i && ((start+j) % i) == 0 ){
+                 sub_array[j] = false;
+            }
+        }
+        if(my_rank == 0){
+             MPI_Gatherv(sub_array, send_counts[my_rank],  MPI_C_BOOL, base_array, send_counts,displs, MPI_C_BOOL, 0, MPI_COMM_WORLD);
+        }
+        else{
+            MPI_Gatherv(sub_array, send_counts[my_rank],  MPI_C_BOOL, NULL, NULL, NULL,  MPI_C_BOOL, 0, MPI_COMM_WORLD);
+        }
+
+    }
+    
+    if (my_rank == 0) {
+        t2 = MPI_Wtime();
+        printf("Array contents \n");
+        for (int i=0; i < N; i++){
+            if(base_array[i]){
+                printf("%d ",i);
+            }
+        }
+            
+        printf("\n");
+        printf("__________________________________\n");
+        printf("Time Elapsed: %f  \n",t2-t1);
+    }
+
     MPI_Finalize();
- 
+    
     return 0;
-}
+    }
