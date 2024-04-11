357a89049e8980bb8cd1307e1e530274be84cc71
Harry Minsky <harry.minsky@gmail.com>
2024-04-11 12:37:48 -0400

    Remove Scatterv as it is outside the scope of the assignment

--- primes.c
+++ primes.c
@@ -22,11 +22,10 @@ bool *createSubArray(int n) {
 
 int main(int argc, char* argv[])
 {
+
     int N = atoi(argv[1])+1;
     
     int my_rank, comm_sz;
-
-    MPI_Status status;
 
     double t1,t2;
     MPI_Init(NULL, NULL);
@@ -34,66 +33,44 @@ int main(int argc, char* argv[])
     MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
     MPI_Comm_size(MPI_COMM_WORLD, &comm_sz);
 
+    int subset_size, offset = 0;
+    if(N < comm_sz){
+        offset = comm_sz-N;
+        subset_size = 1;
+        N = comm_sz;
+    }
+    else{
+        offset = N % comm_sz;
+        N += offset;
+        subset_size = N / comm_sz;
+    }
     bool *base_array = NULL;
     if (my_rank == 0 || true) {
         t1 = MPI_Wtime(); 
         base_array = createBaseArray(N);
     }
-    int *send_counts = malloc(sizeof(int)*comm_sz);
-    int *displs = malloc(sizeof(int)*comm_sz);
-    if(N < comm_sz){
-        for(int i = 0; i < comm_sz; i++){
-            if(i <= N){
-               send_counts[i] = 1;
-            }
-            else{
-               send_counts[i] = 0;
-            }
-        }
-        for(int i = 0; i < comm_sz; i++){
-            displs[i]=0;
-        }
-
-    }
-    else{
-        int subset_offset = N % comm_sz;
-        int trimmed_count = N - subset_offset;
-        int set_size = trimmed_count/comm_sz;
-
-        for(int i = 0; i < comm_sz; i++){
-            if(i == (comm_sz-1) ){
-                send_counts[i] = set_size+subset_offset;
-            }
-            else{
-                send_counts[i] = set_size;
-            }
-        }
-        displs[0] = 0;
-        for(int i = 1; i < comm_sz; i++){
-            displs[i]= displs[i-1] + send_counts[i-1];
-        }
-    }
+    
 
     int limit = (int)ceil(sqrt(N));
     int start;
-    bool *sub_array = createSubArray(send_counts[my_rank]);
+    bool *sub_array = createSubArray(subset_size);
 
     for(int i = 2; i < limit+1; i++){
-        start = displs[my_rank];
+        start = (subset_size*my_rank);
 
-        MPI_Scatterv(base_array, send_counts, displs, MPI_C_BOOL, sub_array,
-            send_counts[my_rank],  MPI_C_BOOL, 0, MPI_COMM_WORLD);
+        MPI_Scatter(base_array, subset_size, MPI_C_BOOL, sub_array,
+            subset_size,  MPI_C_BOOL, 0, MPI_COMM_WORLD);
 
-        for(int j = 0; j < send_counts[my_rank]; j++){
+        for(int j = 0; j < subset_size; j++){
             if(start+j > 2  && (start+j) != i && ((start+j) % i) == 0 ){
                  sub_array[j] = false;
             }
         }
         if(my_rank == 0){
-             MPI_Gatherv(sub_array, send_counts[my_rank],  MPI_C_BOOL, base_array, send_counts,displs, MPI_C_BOOL, 0, MPI_COMM_WORLD);
+             MPI_Gather(sub_array, subset_size,  MPI_C_BOOL, base_array, subset_size, MPI_C_BOOL, 0, MPI_COMM_WORLD);
         }
         else{
-            MPI_Gatherv(sub_array, send_counts[my_rank],  MPI_C_BOOL, NULL, NULL, NULL,  MPI_C_BOOL, 0, MPI_COMM_WORLD);
+            MPI_Gather(sub_array, subset_size,  MPI_C_BOOL, NULL, subset_size, MPI_C_BOOL, 0, MPI_COMM_WORLD);
         }
 
     }
@@ -101,7 +78,7 @@ int main(int argc, char* argv[])
     if (my_rank == 0) {
         t2 = MPI_Wtime();
         printf("Array contents \n");
-        for (int i=0; i < N; i++){
+        for (int i=2; i < N - offset; i++){
             if(base_array[i]){
                 printf("%d ",i);
             }
